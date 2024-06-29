10c5bc0846fdfc5a44746d7cadd4183781ed9516
KkwwwkK <kenwky619@gmail.com>
2024-06-29 11:21:50 +0800

    trying deployment

--- models.py
+++ models.py
@@ -30,25 +30,3 @@ class UserChatRoom(db.Model):
     chat_room_id = db.Column(db.Integer, db.ForeignKey('chat_room.id'), nullable=False)
 
 #
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     password_hash = db.Column(db.String(128), nullable=False)
-#     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-#
-# class ChatRoom(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80), nullable=False)
-#     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-#
-# class Message(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     chat_room_id = db.Column(db.Integer, db.ForeignKey('chat_room.id'), nullable=False)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-#     content = db.Column(db.Text, nullable=False)
-#     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-#
-# class UserChatRoom(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-#     chat_room_id = db.Column(db.Integer, db.ForeignKey('chat_room.id'), nullable=False)
